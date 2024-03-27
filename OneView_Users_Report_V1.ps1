80a48022a40e5e8447bd25289394e6b7228aa49f
Sabri CHARCHOUF <barioucha@gmail.com>
2024-03-27 12:00:11 +0100

    Update script

--- OneView_Users_Report_V1.ps1
+++ OneView_Users_Report_V1.ps1
@@ -1,6 +1,17 @@
-$scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
-$loggingFunctionsPath = Join-Path -Path $scriptPath -ChildPath "..\Logging_Function\Logging_Functions.ps1"
-. $loggingFunctionsPath 
+# Get the directory from which the script is being executed
+$scriptDirectory = $PSScriptRoot
+
+# Construct the path to the Logging_Functions.ps1 script
+$loggingFunctionsPath = Join-Path -Path $scriptDirectory -ChildPath "Logging_Function\Logging_Functions.ps1"
+
+# Check if the Logging_Functions.ps1 script exists
+if (Test-Path -Path $loggingFunctionsPath) {
+    # Dot-source the Logging_Functions.ps1 script
+    . $loggingFunctionsPath
+    Write-Host "Logging functions have been loaded."
+} else {
+    Write-Host "The logging functions script could not be found at: $loggingFunctionsPath"
+}
 # Define the script version
 $ScriptVersion = "1.1"
 # Define the function to import required modules if they are not already imported
@@ -39,161 +50,9 @@ function Import-ModulesIfNotExists {
 }
 # Import the required modules
 Import-ModulesIfNotExists -ModuleNames 'HPEOneView.660', 'Microsoft.PowerShell.Security', 'Microsoft.PowerShell.Utility', 'ImportExcel'
-# Define CSV file name
-$csvFileName = "Appliances_List.csv"
 # Create the full path to the CSV file
-$csvFilePath = Join-Path -Path $scriptPath -ChildPath $csvFileName
+$csvFilePath = Join-Path -Path $scriptDirectory -ChildPath $csvFileName
 # Define the path to the credential folder
-$credentialFolder = Join-Path -Path $scriptPath -ChildPath "credential"
+$credentialFolder = Join-Path -Path $scriptDirectory -ChildPath "credential"
 # Define the path to the credential file
 $credentialFile = Join-Path -Path $credentialFolder -ChildPath "credential.txt"
-# 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-Function Connect-OneViewAppliance {
-    param (
-        [string]$ApplianceFQDN,
-        [PSCredential]$Credential
-    )
-
-    try {
-        # Attempt to connect to the appliance
-        Connect-OVMgmt -Hostname $ApplianceFQDN -Credential $Credential -ErrorAction Stop
-
-        # If the connection is successful, log a success message
-        $message = "Successfully connected to : $ApplianceFQDN"
-        Write-Log -Message $message -Level "OK" -sFullPath $global:sFullPath
-
-        # Log a progress message
-        $message = "Generating report for $ApplianceFQDN..."
-        Write-Log -Message $message -Level "Info" -sFullPath $global:sFullPath
-
-        # Define the base URL for the HPE OneView REST API
-        $baseURL = "https://$ApplianceFQDN/rest"
-
-        # Get the credential object
-        $credential = Get-Credential
-
-        # Convert the username and password from the credential object to a hashtable
-        $credentialHashtable = @{
-            "userName" = $credential.UserName
-            "password" = $credential.GetNetworkCredential().Password
-        }
-
-        # Convert the hashtable to a JSON string
-        $jsonCredential = $credentialHashtable | ConvertTo-Json
-
-        # Get the session ID
-        $response = Invoke-WebRequest -Uri "$baseURL/login-sessions" -Method Post -Body $jsonCredential -ContentType "application/json" -SkipCertificateCheck
-        $sessionID = ($response.Content | ConvertFrom-Json).sessionID
-
-        # Define the headers for the requests
-        $headers = @{
-            "Auth"          = $sessionID
-            "Content-Type"  = "application/json"
-            "X-API-Version" = "4600"
-        }
-
-        # Initialize an array to hold user details
-        $userWithScopes = @()
-
-        # Get all users
-        $users = Invoke-WebRequest -Uri "$baseURL/users" -Method Get -Headers $headers -SkipCertificateCheck
-        $users = $users.Content | ConvertFrom-Json
-
-        # Loop through each user and get details
-
-        # Loop through each user and get details
-        foreach ($user in $users.members) {
-            # Combine user details (modify as needed)
-            $userDetail = New-Object PSObject
-            $userDetail | Add-Member -Type NoteProperty -Name FullName -Value $user.fullName
-            $userDetail | Add-Member -Type NoteProperty -Name UserName -Value $user.userName
-            $userDetail | Add-Member -Type NoteProperty -Name Enabled -Value $user.enabled
-            $userDetail | Add-Member -Type NoteProperty -Name RoleName -Value $user.roleName
-
-            # Add the combined object to the array for further processing
-            $userWithScopes += $userDetail
-        }
-
-        # Define the path to the Excel file
-        $folderPath = Join-Path -Path $scriptPath -ChildPath "Reports"
-        $excelFilePath = Join-Path -Path $folderPath -ChildPath "Users_$ApplianceFQDN.xlsx"
-
-        # Check if the folder exists and create it if it doesn't
-        if (-not (Test-Path -Path $folderPath)) {
-            New-Item -ItemType Directory -Path $folderPath | Out-Null
-            $message = "Reports folder does not exist. Created new folder: $folderPath"
-            Write-Log -Message $message -Level "Info" -sFullPath $global:sFullPath
-        }
-        else {
-            $message = "Reports folder already exists: $folderPath"
-            Write-Log -Message $message -Level "Info" -sFullPath $global:sFullPath
-        }
-
-        # Export user details to the Excel file
-        $userWithScopes | Export-Excel -Path $excelFilePath -AutoSize -AutoFilter -FreezeTopRow
-
-        # Log a completion message
-        $message = "Report for $ApplianceFQDN completed."
-        Write-Log -Message $message -Level "Info" -sFullPath $global:sFullPath
-
-    }
-    catch {
-        # If a connection already exists, log a message and continue
-        if ($_.Exception.Message -like "*already connected*") {
-            $message = "Already connected to : $ApplianceFQDN"
-            Write-Log -Message $message -Level "Info" -sFullPath $global:sFullPath
-        }
-        else {
-            # If the connection fails for any other reason, log an error message
-            $message = "Failed to connect to : $ApplianceFQDN. Error details: $($_.Exception.Message)"
-            Write-Log -Message $message -Level "Error" -sFullPath $global:sFullPath
-        }    
-    }
-}
-# Check if the credential folder exists, if not, create it
-if (!(Test-Path -Path $credentialFolder)) {
-    Write-Log -Message "The credential folder $credentialFolder does not exist. Create it now..." -Level "Warning" -sFullPath $global:sFullPath
-    New-Item -ItemType Directory -Path $credentialFolder | Out-Null
-    Write-Log -Message "The credential folder $credentialFolder has been created successfully." -Level "OK" -sFullPath $global:sFullPath
-}
-
-# If the credential file exists, try to load the credential from it
-if (Test-Path -Path $credentialFile) {
-    try {
-        $credential = Import-Clixml -Path $credentialFile
-    }
-    catch {
-        Write-Host "Error loading credential file. Please enter your credentials."
-        $credential = Get-Credential -Message "Enter your username and password"
-        $credential | Export-Clixml -Path $credentialFile
-    }
-}
-else {
-    # If the credential file doesn't exist, ask for the username and password and store them in the credential file
-    $credential = Get-Credential -Message "Enter your username and password"
-    $credential | Export-Clixml -Path $credentialFile
-}
-
-# Import the CSV file and connect to each appliance
-Import-Csv -Path $csvFilePath | ForEach-Object {
-    Connect-OneViewAppliance -ApplianceFQDN $_.FQDN -Credential $credential
-}
