0c25b2a2f57d2602f30df3fd87e15cadc6baf312
Sabri CHARCHOUF <barioucha@gmail.com>
2024-03-29 13:53:56 +0100

    Replace * Add Script Header

--- OneView_Users_Report_V1.ps1
+++ OneView_Users_Report_V1.ps1
@@ -1,13 +1,22 @@
 # Define the script version
 $ScriptVersion = "1.0"
-# Retrieve the script name dynamically
-$ScriptName = $MyInvocation.MyCommand.Name
 # Get the directory from which the script is being executed
 $scriptDirectory = $PSScriptRoot
 # Move up one level from the current script directory and then into the Logging_Function directory
 $loggingFunctionsDirectory = Join-Path -Path $scriptDirectory -ChildPath "..\Logging_Function"
 # Construct the path to the Logging_Functions.ps1 script
 $loggingFunctionsPath = Join-Path -Path $loggingFunctionsDirectory -ChildPath "Logging_Functions.ps1"
+# Script Header
+# Get the width of the console window
+$consoleWidth = $host.UI.RawUI.WindowSize.Width
+# Create a string of "=" characters that is as long as the console is wide
+$line = "=" * ($consoleWidth - 1)
+# Script Header
+Write-Host "`n$line" -ForegroundColor Cyan
+Write-Host "  MY SCRIPT" -ForegroundColor Yellow
+Write-Host "  Version: $ScriptVersion" -ForegroundColor Yellow
+Write-Host "  Description: This script does amazing things!" -ForegroundColor Yellow
+Write-Host "$line`n" -ForegroundColor Cyan
 # Check if the Logging_Functions.ps1 script exists
 if (Test-Path -Path $loggingFunctionsPath) {
     # Dot-source the Logging_Functions.ps1 script
@@ -30,7 +39,7 @@ function Import-ModulesIfNotExists {
     $taskNumber = 1
 
     # Task 1: Checking required modules
-    Write-Host "`n$($taskNumber). Checking required modules:`n" -ForegroundColor Cyan
+    Write-Host "`n$($taskNumber). Checking required modules:`n" -ForegroundColor Magenta
     Write-Log -Message "Checking required modules." -Level "Info"
     $taskNumber++
 
@@ -44,31 +53,31 @@ function Import-ModulesIfNotExists {
         try {
             # Check if the module is installed
             if (-not (Get-Module -ListAvailable -Name $ModuleName)) {
-                Write-Host "`t➡️ Module " -NoNewline
+                Write-Host "`t* Module " -NoNewline -ForegroundColor White
                 Write-Host "[$ModuleName]" -NoNewline -ForegroundColor Red
-                Write-Host " is not installed." -ForegroundColor Red
+                Write-Host " is not installed." -ForegroundColor White
                 Write-Log -Message "Module '[$ModuleName]' is not installed." -Level "Error" -NoConsoleOutput
                 continue
             }
 
             # Check if the module is already imported
             if (Get-Module -Name $ModuleName) {
-                Write-Host "`t➡️ Module " -NoNewline
+                Write-Host "`t* Module " -NoNewline -ForegroundColor White
                 Write-Host "[$ModuleName]" -NoNewline -ForegroundColor Yellow
-                Write-Host " is already imported." -ForegroundColor Yellow
+                Write-Host " is already imported." -ForegroundColor White
                 Write-Log -Message "Module '[$ModuleName]' is already imported." -Level "Info" -NoConsoleOutput
                 continue
             }
 
             # Try to import the module
             Import-Module $ModuleName -ErrorAction Stop
-            Write-Host "`t➡️ Module " -NoNewline
+            Write-Host "`t* Module " -NoNewline -ForegroundColor White
             Write-Host "[$ModuleName]" -NoNewline -ForegroundColor Green
-            Write-Host " imported successfully." -ForegroundColor Green
+            Write-Host " imported successfully." -ForegroundColor White
             Write-Log -Message "Module '[$ModuleName]' imported successfully." -Level "OK" -NoConsoleOutput
         }
         catch {
-            Write-Host "`t➡️ Failed to import module " -NoNewline
+            Write-Host "`t* Failed to import module " -NoNewline
             Write-Host "[$ModuleName]" -NoNewline -ForegroundColor Red
             Write-Host ": $_" -ForegroundColor Red
             Write-Log -Message "Failed to import module '[$ModuleName]': $_" -Level "Error" -NoConsoleOutput
