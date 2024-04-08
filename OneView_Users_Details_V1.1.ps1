dc8515f1bcdf1e5ebdcdf6c2b5920f28c8f36e7d
Sabri CHARCHOUF <barioucha@gmail.com>
2024-04-08 17:44:12 +0200

    Back to old version

--- OneView_Users_Details_V1.1.ps1
+++ OneView_Users_Details_V1.1.ps1
@@ -350,34 +350,28 @@ function Close-ExcelFile {
         [ValidateNotNullOrEmpty()]
         [string]$ExcelFilePath
     )
-
-    # Create a new Excel COM object
-    $excel = New-Object -ComObject Excel.Application
-
-    # Hide the Excel application
-    $excel.Visible = $false
-
-    # Iterate over all open workbooks
-    foreach ($workbook in $excel.Workbooks) {
-        # If the workbook's full name matches the file path, close it
-        if ($workbook.FullName -eq $ExcelFilePath) {
-            $workbook.Close($false)  # Don't save changes
+    try {
+        # Try to open the file in ReadWrite mode
+        $fileStream = [System.IO.File]::Open($ExcelFilePath, [System.IO.FileMode]::Open, [System.IO.FileAccess]::ReadWrite, [System.IO.FileShare]::None)
+        if ($fileStream) {
+            $fileStream.Close()
+            Write-Host "`t• " -NoNewline -ForegroundColor White
+            Write-Host "The Excel file was already closed.`n" -ForegroundColor Yellow
+            Write-Log -Message "The Excel file was already closed." -Level "Info" -NoConsoleOutput
+        }
+    }
+    catch {
+        # If an exception is thrown, the file is open
+        $excelFile = Get-Process | Where-Object { $_.MainWindowTitle -like "*Excel*" }
+        if ($excelFile) {
+            # Close the Excel file
+            $excelFile | Stop-Process -Force
             Write-Host "`t• " -NoNewline -ForegroundColor White
             Write-Host "The Excel file was open and has been closed.`n" -ForegroundColor Green
             Write-Log -Message "The Excel file was open and has been closed." -Level "OK" -NoConsoleOutput
-            break
         }
     }
-
-    # Quit the Excel application
-    $excel.Quit()
-
-    # Release the COM object
-    [System.Runtime.Interopservices.Marshal]::ReleaseComObject($excel) | Out-Null
-    [System.GC]::Collect()
-    [System.GC]::WaitForPendingFinalizers()
 }
-
 # Close the Excel file if it is open
 Close-ExcelFile -ExcelFilePath $combinedUsersExcelPath
 # Add a delay to ensure the Excel file is closed before exporting the data
@@ -385,27 +379,33 @@ Start-Sleep -Seconds 5
 # Sort the combined users by ApplianceConnection and then by userName
 $sortedCombinedUsers = $combinedUsers | Sort-Object ApplianceConnection, type
 # Export the data to Excel
-$sortedCombinedUsers | Export-Excel -Path $combinedUsersExcelPath `
+$excel = $sortedCombinedUsers | Export-Excel -Path $combinedUsersExcelPath `
     -ClearSheet `
     -AutoSize `
     -AutoFilter `
     -FreezeTopRow `
     -WorksheetName "CombinedUsers" `
     -TableStyle "Medium11" `
-    # Add a delay to give Export-Excel time to finish writing the file
-    Start-Sleep -Seconds 5
-# Confirm that the Excel file was created successfully
-if (Test-Path -Path $combinedUsersExcelPath) {
+    -PassThru
+# Import the Worksheet that include User roles and permissions information
+$UserRolesWorksheet = Import-Excel -Path ".\User_Roles_Permissions\User_Roles_Permissions.xlsx" -WorksheetName "User_Roles"
+# Add the User Role Worksheet to the target Excel file
+$excel = $excel | Add-Worksheet -WorksheetName "UserRoles" -DataTable $UserRolesWorksheet
+    # Check if the Excel file was created successfully
+if ($excel) {
     Write-Host "`t• " -NoNewline -ForegroundColor White
-    Write-Host "The Excel file was created successfully at:" -NoNewline -ForegroundColor DarkGray
-    Write-Host " $combinedUsersExcelPath" -ForegroundColor Green
-    Write-Log -Message "The Excel file was created successfully at $combinedUsersExcelPath" -Level "OK" -NoConsoleOutput
+    Write-Host "The Excel file was created successfully.`n" -ForegroundColor Green
+    Write-Log -Message "The Excel file was created successfully." -Level "OK" -NoConsoleOutput
 }
 else {
     Write-Host "`t• " -NoNewline -ForegroundColor White
-    Write-Host "Failed to create the Excel file." -ForegroundColor Red
+    Write-Host "Failed to create the Excel file.`n" -ForegroundColor Red
     Write-Log -Message "Failed to create the Excel file." -Level "Error" -NoConsoleOutput
 }
+# Save and close the Excel package
+$excel.Save()
+$excel.Dispose()
+Write-Host "+$line+" -ForegroundColor DarkGray
 # Increment $script:taskNumber after the function call
 $script:taskNumber++
 # Task 7: Script execution completed successfully
@@ -416,5 +416,14 @@ Write-Log -Message "Script execution completed successfully." -Level "OK" -NoCon
 # Just before calling Complete-Logging
 $endTime = Get-Date
 $totalRuntime = $endTime - $startTime
+# Complettion Date & Time based on Complete-Logging function
+$completionTime = Get-Date -Format "dd/MM/yyyy HH:mm:ss"
+# Write the completion time to the console
+Write-Host "`n$Spaces$($taskNumber). Script completion time:" -NoNewline -ForegroundColor DarkGray
+Write-Host "[$completionTime]" -ForegroundColor Cyan
+# Say if the script execution was successful using the Complete-Logging function
+
+Write-Host "[$completionTime]"
+
 # Call Complete-Logging at the end of the script
 Complete-Logging -LogPath $script:LogPath -ErrorCount $ErrorCount -WarningCount $WarningCount -TotalRuntime $totalRuntime
